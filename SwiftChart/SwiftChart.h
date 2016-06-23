80d2e3ab991dd449e6d8ef080ccb257b5c3ddf3e
Tino Heth <th@t-no.de>
2016-06-23 09:27:38 +0200

    Initial Commit

---
+++ SwiftChart/SwiftChart.h
@@ -0,0 +1,19 @@
+//
+//  SwiftChart.h
+//  SwiftChart
+//
+//  Created by Tino Heth on 23.06.16.
+//  Copyright © 2016 Tino Heth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftChart.
+FOUNDATION_EXPORT double SwiftChartVersionNumber;
+
+//! Project version string for SwiftChart.
+FOUNDATION_EXPORT const unsigned char SwiftChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftChart/PublicHeader.h>
+
+
